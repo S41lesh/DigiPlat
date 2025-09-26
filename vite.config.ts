a49ae96979b81befb11d517f97dc9ddeef22ec7c
S41lesh <sailesh.g.n@gmail.com>
2025-09-27 00:10:04 +0530

    changes v3

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/DigiPlat/" : "/",
+export default defineConfig(() => ({
+  base: "/DigiPlat/", // 👈 force assets to load from /DigiPlat/
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
